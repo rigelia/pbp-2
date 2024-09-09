d697fb5cbf08c3ebfcf37d10a0a9d25a9ed6f347
rigelia <f3atha04@gmail.com>
2024-09-09 22:24:43 +0700

    updated allowed hosts

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*=hu*u5@q$ftdppn-^@pjaqwg*pszswm+xog971@6a_i!t4d%(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://pbp.cs.ui.ac.id/fadiansah.feryan/tugas2"]
 
 
 # Application definition
