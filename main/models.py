d88d4aa6cc3a6e94538d7d811da5edec0a7aab16
rigelia <f3atha04@gmail.com>
2024-09-22 21:55:03 +0700

    tugas 4 initial commit

--- main/models.py
+++ main/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
     price = models.IntegerField()
     description = models.TextField()
